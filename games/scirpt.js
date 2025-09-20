ebab7e6a08e7c0b367661fa80c602d0f3bcc03ed
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-09-20 12:32:48 +0530

    Delete games directory

--- games/scirpt.js
+++
@@ -1,9 +0,0 @@
-// Redirect user to game URL when card is clicked
-document.querySelectorAll('.game-card').forEach(card => {
-  card.addEventListener('click', () => {
-    const url = card.getAttribute('data-url');
-    if (url) {
-      window.open(url, "_blank"); // opens in new tab
-    }
-  });
-});
